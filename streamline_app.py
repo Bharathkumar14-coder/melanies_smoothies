76b6dac9543def037c1176698e8569865ca1658f
Bharathkumar14-coder <bharathkmr14@gmail.com>
2024-07-14 17:04:41 +0530

    Update streamline_app.py
    Testing

--- streamline_app.py
+++ streamline_app.py
@@ -15,7 +15,12 @@ st.write("The name on your smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#convert snowflake dataframe to pandas dataframe
+pd_df=my_dateframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
