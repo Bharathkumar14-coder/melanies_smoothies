0b719dc9fd63d2ebeb258bfce98162c0d7427ed7
Bharathkumar14-coder <bharathkmr14@gmail.com>
2024-07-14 17:17:06 +0530

    Update streamline_app.py
    syntax_error

--- streamline_app.py
+++ streamline_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 #st.stop()
 
 #convert snowflake dataframe to pandas dataframe
-pd_df=my_dataframe.to_pandas()
+pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
